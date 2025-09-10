d2cd76356af0928cf2678168ef42319217729055
audreym101 <m.audrey1@alustudent.com>
2025-09-10 10:32:44 -0700

    str attachement

---
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string str1 = "Welcome to";
+        string str2 = "Holberton School!";
+        Console.WriteLine(str1 + " " + str2);
+    }
+}
