61a6e973a81f05173d72742623614f980bd1301c
audreym101 <m.audrey1@alustudent.com>
2025-09-10 11:25:40 -0700

    +- and 0

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        double percent = 0.7553;      // 75.53%
-        double currency = 98765.4321;  // $98,765.43
-        Console.WriteLine($"Percent: {percent:P2} tip"); 
+        double percent = 0.2641;      
+        double currency = 12345.678;  
+        Console.WriteLine($"Percent: {percent:P2} tip");
         Console.WriteLine($"Currency: {currency:C2}");
     }
 }
