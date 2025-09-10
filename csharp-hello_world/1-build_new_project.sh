2137630985128bf27faca23f79081c9a066be27b
audreym101 <m.audrey1@alustudent.com>
2025-09-10 15:43:05 -0700

    hello w

--- csharp-hello_world/1-build_new_project.sh
+++ csharp-hello_world/1-build_new_project.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-# Create the project directory
-mkdir -p 1-new_project
-
-# Navigate to the project directory
-cd 1-new_project
-
-# Create a new console application
-dotnet new console > /dev/null 2>&1
+# Create a new console application in 1-new_project directory
+dotnet new console -o 1-new_project > /dev/null 2>&1
 
 # Build the project
-dotnet build > /dev/null 2>&1
+dotnet build 1-new_project
