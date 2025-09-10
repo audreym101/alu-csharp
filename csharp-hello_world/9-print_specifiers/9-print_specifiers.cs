f90c281529063fedd34472e7585e57dbfa0df10d
audreym101 <m.audrey1@alustudent.com>
2025-09-10 16:55:56 -0700

    +-,0

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Globalization;
 class Program
 {
     static void Main(string[] args)
     {
         double percent = .7553;
         double currency = 98765.4321;
-        Console.WriteLine($"Percent: {percent:P}");
+        Console.WriteLine($"Percent: {percent:P2}");
         Console.WriteLine($"Currency: {currency:C2}");
     }
 }
