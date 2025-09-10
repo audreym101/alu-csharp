22d783ba4a018c2ebd69885ac5c1c13795ef2735
audreym101 <m.audrey1@alustudent.com>
2025-09-10 10:49:28 -0700

    str and num

---
+++ csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 972;
+        string str1 = "Mission";
+        string str2 = "Street";
+        Console.WriteLine($"{number} {str1} {str2}");
+    }
+}
