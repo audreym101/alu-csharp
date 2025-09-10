b83d6c8889e54c30305e262a7b68341ec528aa67
audreym101 <m.audrey1@alustudent.com>
2025-09-10 11:11:30 -0700

    double usage

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,28 +1,12 @@
 ﻿using System;
-using System.Globalization;
-
 class Program
 {
-    static void Main()
-    {
-        double percent = 0.7553;
-        double amount = 98765.43;
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
 
-        // Percent output
-        Console.WriteLine("Percent: {0:P2} tip", percent);
-        
-        // Currency output
-        Console.WriteLine("Currency: {0:C2}", amount);
-
-        // Additional test cases
-        percent = -0.7553;
-        amount = -98765.43;
-        Console.WriteLine("Percent: {0:P2} tip", percent);
-        Console.WriteLine("Currency: {0:C2}", amount);
-
-        percent = 0;
-        amount = 0;
-        Console.WriteLine("Percent: {0:P2} tip", percent);
-        Console.WriteLine("Currency: {0:C2}", amount);
-    }
+		Console.WriteLine("Percent: {0:P2} tip", percent);
+		Console.WriteLine("Currency: {0:C2}", currency);
+	}
 }
