5f6ad3cc321ddcbff29bafb22fd79b71512562d6
audreym101 <m.audrey1@alustudent.com>
2025-09-10 09:48:28 -0700

    num +"anyword"

---
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = 972;
+        Console.WriteLine(number + " Mission Street");
+    }
+}
+// The above code prints an integer followed by a string to the console.    
