4eb5d2b58bf5461fb69f639525ec5852673b7390
audreym101 <m.audrey1@alustudent.com>
2025-09-12 14:30:51 -0700

    string words  attachement

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -1,11 +1,11 @@
 ﻿using System;
-
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine(
-            true.GetHashCode()
+            string.Concat("object", "-", "oriented", " ", "programming", " ", "in", " ", "C#")
         );
     }
 }
+// Output: object-oriented programming in C#
