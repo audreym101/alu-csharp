ebd8cc84a30c8b1bd7d6cd6cf1b631d5965db393
audreym101 <m.audrey1@alustudent.com>
2025-09-12 15:46:42 -0700

    printing positive, negative and zero

---
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // DO NOT TOUCH THIS
+        Random rand = new Random();
+        int number = rand.Next(-10, 11); // assigns a random number between -10 and 10
+
+        // YOUR CODE BELOW
+        if (number > 0)
+        {
+            Console.WriteLine($"{number} is positive");
+        }
+        else if (number < 0)
+        {
+            Console.WriteLine($"{number} is negative");
+        }
+        else
+        {
+            Console.WriteLine($"{number} is zero");
+        }
+    }
+}
