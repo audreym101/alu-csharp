ab379db782bfd9a72e4744921c8bd6ef9f359bd4
audreym101 <m.audrey1@alustudent.com>
2025-09-12 15:58:33 -0700

    creating a positive, negative and zero number

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -4,22 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        // DO NOT TOUCH THIS
-        Random rand = new Random();
-        int number = rand.Next(-10, 11); // assigns a random number between -10 and 10
+        // For testing purposes, you can assign these values:
+        // int number = 98;   // positive test
+        // int number = -98;  // negative test
+        // int number = 0;    // zero test
 
-        // YOUR CODE BELOW
+        int number = int.Parse(Console.ReadLine()); // optional: read input for testing
+
         if (number > 0)
-        {
             Console.WriteLine($"{number} is positive");
-        }
         else if (number < 0)
-        {
             Console.WriteLine($"{number} is negative");
-        }
         else
-        {
             Console.WriteLine($"{number} is zero");
-        }
     }
 }
