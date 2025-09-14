23c0c7feb0d304992ed8ea5018683c5f24487961
audreym101 <m.audrey1@alustudent.com>
2025-09-14 06:57:46 -0700

    if else condition

--- csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
+++ csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -8,16 +8,16 @@ class Program
         int number = rnd.Next(-10, 10); // Do not touch this line
 
         if (number > 0)
-        {
+        
             Console.WriteLine($"{number} is positive");
-        }
+        
         else if (number == 0)
-        {
+        
             Console.WriteLine($"{number} is zero");
-        }
+        
         else
-        {
+        
             Console.WriteLine($"{number} is negative");
-        }
+        
     }
 }
