9c5e4910156a7cb7dba8dd4cff020b28c593665d
audreym101 <m.audrey1@alustudent.com>
2025-09-14 10:04:01 -0700

    ascending order

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string numbers = "";
+        for (int i = 0; i <= 99; i++)
+        {
+            numbers += i.ToString("D2"); // Format as 2 digits
+            if (i != 99)                 // Add separator for all but the last
+                numbers += ", ";
+        }
+        Console.WriteLine(numbers);      // Only one write needed, prints newline at end
+    }
+}
