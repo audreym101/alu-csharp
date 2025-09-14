db6891bf236bf30f37b58778975e77590616de10
audreym101 <m.audrey1@alustudent.com>
2025-09-14 10:53:26 -0700

    last digits

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -4,27 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[] testNumbers = { 98, -98, 0 };
+        Random random = new Random();
+        int number = random.Next(-10000, 10000);
+        int lastDigit = Math.Abs(number % 10);
 
-        foreach (int number in testNumbers)
-        {
-            int lastDigit = number % 10;
-            string message;
+        Console.Write($"The last digit of {number} is {lastDigit} and is ");
 
-            if (lastDigit > 5)
-            {
-                message = "and is greater than 5";
-            }
-            else if (lastDigit == 0)
-            {
-                message = "and is 0";
-            }
-            else
-            {
-                message = "and is less than 6 and not 0";
-            }
-
-            Console.WriteLine($"The last digit of {number} is {lastDigit} {message}");
-        }
+        if (lastDigit > 5)
+            Console.WriteLine("greater than 5");
+        else if (lastDigit == 0)
+            Console.WriteLine("0");
+        else
+            Console.WriteLine("less than 6 and not 0");
     }
 }
+// This program generates a random integer between -10000 and 10000,    
