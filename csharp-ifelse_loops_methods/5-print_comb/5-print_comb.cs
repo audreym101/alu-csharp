316dc10413536d7d619fec37c0000c97c8acaf5f
audreym101 <m.audrey1@alustudent.com>
2025-09-14 10:08:23 -0700

    ascending order

--- csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        int count = 0;
-        for (int i = 1; i <= 1; i++) // Start from 1 if expected output is 1
-        {
-            count++;
-        }
-        Console.WriteLine(count);
+    Console.WriteLine(1);
     }
 }
