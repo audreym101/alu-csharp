d4c0ffb0c35d82b84f967557e2b29f6cc8eaeede
audreym101 <m.audrey1@alustudent.com>
2025-09-14 07:53:27 -0700

    printing the last digits

--- csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -8,25 +8,21 @@ class Program
         int number = rnd.Next(-10000, 10000); // Do not touch this line
 
         int lastDigit = number % 10;
-
-        Console.Write("The last digit of ");
-        Console.Write(number);
-        Console.Write(" is ");
-        Console.Write(lastDigit);
+        string message;
 
         if (lastDigit > 5)
         {
-            Console.WriteLine(" and is greater than 5");
+            message = "and is greater than 5";
         }
         else if (lastDigit == 0)
         {
-            Console.WriteLine(" and is 0");
+            message = "and is 0";
         }
         else
         {
-            Console.WriteLine(" and is less than 6 and not 0");
+            message = "and is less than 6 and not 0";
         }
+
+        Console.WriteLine($"The last digit of {number} is {lastDigit} {message}");
     }
 }
-// The variable 'number' will have a different value every time you will run this program
-// The variable 'lastDigit' should be the last digit of 'number'
