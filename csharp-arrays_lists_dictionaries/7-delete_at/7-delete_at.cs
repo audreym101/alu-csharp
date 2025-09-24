ac2ad3fa09ef03c3661f7b32190a6b1bf8e45fa3
audreym101 <m.audrey1@alustudent.com>
2025-09-24 01:35:20 -0700

    deletion  when removeAt isnt alllowed

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        // Shift elements left
+        for (int i = index; i < myList.Count - 1; i++)
+        {
+            myList[i] = myList[i + 1];
+        }
+
+        // Remove the last element (duplicate)
+        myList.RemoveAt(myList.Count - 1); // If not allowed, see alternative below
+
+        return myList;
+    }
+}
+        // Alternative if RemoveAt is not allowed:
+        // myList.RemoveAt(myList.Count - 1); // Remove the last element (duplicate)
+        // return myList.GetRange(0, myList.Count - 1); // Return a new list without the last element
