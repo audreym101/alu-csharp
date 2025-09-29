7542380598d58e51dbaa233d1c92a07bad926c50
audreym101 <m.audrey1@alustudent.com>
2025-09-29 01:56:20 -0700

    maxvalue

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(System.Collections.Generic.Dictionary<string, int> myList)
+    {
+        if (myList == null || myList.Count == 0)
+            return "None";
+
+        string bestKey = null;
+        int maxValue = int.MinValue;
+
+        foreach (var entry in myList)
+        {
+            if (entry.Value > maxValue)
+            {
+                maxValue = entry.Value;
+                bestKey = entry.Key;
+            }
+        }
+
+        return bestKey;
+    }
+}
