b3cbd32e7265782924c2c1b114b05cb96bacb477
audreym101 <m.audrey1@alustudent.com>
2025-09-29 00:49:37 -0700

    concat

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        string str1 = "Welcome to";
+        string str1 = "Welcome to ";
         string str2 = "Holberton School!";
-        Console.WriteLine(str1 + " " + str2);
+        Console.WriteLine($"{str1}{str2}");
     }
 }
