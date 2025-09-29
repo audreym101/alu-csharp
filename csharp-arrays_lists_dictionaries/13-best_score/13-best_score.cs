adafc6c12e3b6d21f3d7759e7173af2399c4fd1e
audreym101 <m.audrey1@alustudent.com>
2025-09-29 02:06:29 -0700

    changes

--- csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -1,25 +1,25 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 class Dictionary
 {
-    public static string BestScore(System.Collections.Generic.Dictionary<string, int> myList)
+    public static string BestScore(Dictionary<string, int> myList)
     {
         if (myList == null || myList.Count == 0)
             return "None";
-
-        string bestKey = null;
+        
+        string? bestKey = null;
         int maxValue = int.MinValue;
-
-        foreach (var entry in myList)
+        
+        foreach (var kvp in myList)
         {
-            if (entry.Value > maxValue)
+            if (kvp.Value > maxValue)
             {
-                maxValue = entry.Value;
-                bestKey = entry.Key;
+                maxValue = kvp.Value;
+                bestKey = kvp.Key;
             }
         }
-
-        return bestKey;
+        
+        return bestKey ?? "None";
     }
 }
