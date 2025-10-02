95cca5a24f8b4d564d2accc8c50099bc1969cabb
audreym101 <m.audrey1@alustudent.com>
2025-10-02 16:00:46 -0700

    matrix

--- csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Required output for the checker
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+public class Matrix
+{
+    public static int[,] Square(int[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        int[,] result = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+            for (int j = 0; j < cols; j++)
+                result[i, j] = matrix[i, j] * matrix[i, j];
+
+        return result;
+    }
+}
+
