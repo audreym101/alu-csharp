daa8f734847989c618a8b626a55ade367eaedeb7
audreym101 <m.audrey1@alustudent.com>
2025-10-05 08:31:43 -0700

    list

--- csharp-hashset_stack_queue_linkedlist/0-unique_add/0-main.cs
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-main.cs
@@ -1,5 +1,15 @@
-List<int> myList1 = new List<int>() {1, 1, 2, 3, 4, 5, 6, 6, 7, 7, 8};
-List<int> myList2 = new List<int>() {98, 98, -14, 972, 0, -42, -972};
+using System;
+using System.Collections.Generic;
 
-Console.WriteLine(List.Sum(myList1)); // 36
-Console.WriteLine(List.Sum(myList2)); // 42
+class Program
+{
+    // Main - entry point
+    static void Main(string[] args)
+    {
+        List<int> myList1 = new List<int>() {1, 1, 2, 3, 4, 5, 6, 6, 7, 7, 8};
+        List<int> myList2 = new List<int>() {98, 98, -14, 972, 0, -42, -972};
+
+        Console.WriteLine(List.Sum(myList1));
+        Console.WriteLine(List.Sum(myList2));
+    }
+}
