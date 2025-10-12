2119bc48b064615ac58a75fd2cd935b1e9a287bf
audreym101 <m.audrey1@alustudent.com>
2025-10-11 17:28:06 -0700

    exception

--- csharp-exceptions/2-divide_lists/2-main.cs
+++ csharp-exceptions/2-divide_lists/2-main.cs
@@ -1,17 +1,30 @@
+// main_0.cs
 using System;
-using System.Collections.Generic;
 
-class Program
+class Main0
 {
     static void Main(string[] args)
     {
-        List<int> list1 = new List<int>() {1, 20, 16, 15, 54};
-        List<int> list2 = new List<int>() {1, 0, 2, 3};
-        List<int> result;
+        int[] list1 = {10, 0, 4};
+        int[] list2 = {2, 4, 0};
 
-        result = List.Divide(list1, list2, 5);
-
-        foreach (int i in result)
-            Console.WriteLine(i);
+        for (int i = 0; i < Math.Max(list1.Length, list2.Length); i++)
+        {
+            try
+            {
+                Console.WriteLine(list1[i] / list2[i]);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
     }
 }
+
+// main_1.cs → class Main1 { ... }
+// main_2.cs → class Main2 { ... }
