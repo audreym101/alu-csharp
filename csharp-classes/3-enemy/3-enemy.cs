a53898ba3a2b55bd6ffb7349fe67e9e68f0ac82b
audreym101 <m.audrey1@alustudent.com>
2025-10-26 11:35:51 -0700

    classes tasks

--- csharp-classes/3-enemy/3-enemy.cs
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -2,22 +2,15 @@ using System;
 
 namespace Enemies
 {
-    /// <summary>
-    /// Defines a zombie with private health and accessors.
-    /// </summary>
     public class Zombie
     {
         private int health;
 
-        /// <summary>Initializes a new Zombie with 0 health.</summary>
         public Zombie()
         {
             health = 0;
         }
 
-        /// <summary>Initializes a new Zombie with the specified health.</summary>
-        /// <param name="value">Health value (must be >= 0).</param>
-        /// <exception cref="ArgumentException">Thrown when value is negative.</exception>
         public Zombie(int value)
         {
             if (value < 0)
@@ -25,7 +18,6 @@ namespace Enemies
             health = value;
         }
 
-        /// <summary>Returns the current health.</summary>
         public int GetHealth()
         {
             return health;
