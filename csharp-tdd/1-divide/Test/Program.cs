f7fcc0de68e94c4307bf7ecd623292e8841c9391
audreym101 <m.audrey1@alustudent.com>
2025-11-02 10:27:31 -0800

    tdd

--- csharp-tdd/1-divide/Test/Program.cs
+++ csharp-tdd/1-divide/Test/Program.cs
@@ -7,10 +7,10 @@ class Program
     {
         if (args.Length == 0)
         {
-            // Test case 1: {{98, 0, -12}, {21, 972, 44}, {-727, 60, -2}} / 2
-            int[,] matrix1 = { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
-            int[,] result1 = Matrix.Divide(matrix1, 2);
-            PrintMatrix(result1);
+            // Default: Test case 1
+            int[,] matrix = { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
+            int[,] result = Matrix.Divide(matrix, 2);
+            PrintMatrix(result);
             return;
         }
 
@@ -19,27 +19,23 @@ class Program
         switch (testCase)
         {
             case "1":
-                // Test case 1: {{98, 0, -12}, {21, 972, 44}, {-727, 60, -2}} / 2
                 int[,] matrix1 = { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
                 int[,] result1 = Matrix.Divide(matrix1, 2);
                 PrintMatrix(result1);
                 break;
                 
             case "2":
-                // Test case 2: {{98, 0, -12}, {21, 972, 44}, {-727, 60, -2}} / -7
                 int[,] matrix2 = { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
                 int[,] result2 = Matrix.Divide(matrix2, -7);
                 PrintMatrix(result2);
                 break;
                 
             case "3":
-                // Test case 3: {{98, 0, -12}, {21, 972, 44}, {-727, 60, -2}} / 0
                 int[,] matrix3 = { { 98, 0, -12 }, { 21, 972, 44 }, { -727, 60, -2 } };
-                int[,] result3 = Matrix.Divide(matrix3, 0);
+                Matrix.Divide(matrix3, 0);
                 break;
                 
             case "4":
-                // Test case 4: empty matrix / 4
                 int[,] matrix4 = new int[6, 6];
                 int[,] result4 = Matrix.Divide(matrix4, 4);
                 PrintMatrix(result4);
