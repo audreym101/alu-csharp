ae34da188d776f947b3ec48f2936e436d2c4942f
audreym101 <m.audrey1@alustudent.com>
2025-11-16 09:03:01 -0800

    Update matrix addition: public class, null checks, 2x2 and 3x3 validation

--- csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -1,11 +1,15 @@
-class MatrixMath
+public class MatrixMath
 {
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
+        if (matrix1 == null || matrix2 == null)
+            return new double[,] { { -1 } };
+        
         int rows1 = matrix1.GetLength(0), cols1 = matrix1.GetLength(1);
         int rows2 = matrix2.GetLength(0), cols2 = matrix2.GetLength(1);
         
-        if (rows1 != rows2 || cols1 != cols2 || (rows1 != 2 && rows1 != 3) || (cols1 != 2 && cols1 != 3))
+        if (rows1 != rows2 || cols1 != cols2 || 
+            !((rows1 == 2 && cols1 == 2) || (rows1 == 3 && cols1 == 3)))
             return new double[,] { { -1 } };
         
         double[,] result = new double[rows1, cols1];
