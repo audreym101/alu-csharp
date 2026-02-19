f969cf79de5f52c5bdca025aadb1c1da028747a5
audreym101 <m.audrey1@alustudent.com>
2026-02-18 22:33:58 -0800

    Add abstract Base class

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+using System;
+
+/// <summary>
+/// Abstract base class
+/// </summary>
+public abstract class Base
+{
+    public string name { get; set; }
+
+    public override string ToString()
+    {
+        return $"{name} is a {this.GetType().Name}";
+    }
+}
